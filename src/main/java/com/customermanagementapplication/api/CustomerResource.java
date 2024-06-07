4edbfbb99f909ab4e603b77dc3dcbf93369f59ce
basilbab <basilbab@gmail.com>
2024-06-07 15:34:22 +0530

    Injection in progress

--- src/main/java/com/customermanagementapplication/api/CustomerResource.java
+++ src/main/java/com/customermanagementapplication/api/CustomerResource.java
@@ -8,6 +8,8 @@ import com.customermanagementapplication.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CustomerResource {
     @Autowired
     private CustomerService customerService;
+    @PostMapping
+      
     public Customer addCustomer(@RequestBody Customer customer)
     {
         
